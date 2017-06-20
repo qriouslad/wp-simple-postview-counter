bfc7e5c94cd911c6b5cb9afd6c7229ed29e6ea1d
CodeGood <codegood.io@gmail.com>
2017-06-20 07:27:06 +0000

    Initial commit: plugin definition.

---
+++ wp-simple-postview-counter.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: WP Simple Post View Counter
+Plugin URI: https://github.com/qriouslad/wp-simple-postview-counter
+Description: Simple post view counter based on tutorial at https://www.smashingmagazine.com/2011/09/how-to-create-a-wordpress-plugin/
+Version: 1.0
+Author: Bowo
+Author URI: https://bowo.io
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+WP Simple Post View Counter is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+WP Simple Post View Counter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with WP Simple JQuery Slider. If not, see https://www.gnu.org/licenses/gpl-2.0.html
+*/
